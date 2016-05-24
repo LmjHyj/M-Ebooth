5ff670560d04f0c7ca14520ac8ac163eb9bbffa4
eagune <jingdiqkl123@qq.com>
2016-05-24 20:14:21 +0800

    Signed-off-by: eagune <jingdiqkl123@qq.com>

--- app.js
+++ app.js
@@ -10,6 +10,43 @@ var users = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');    //引用mongoose模块
+var db = mongoose.createConnection('localhost','mebooth'); //创建一个数据库连接
+db.on('error',console.error.bind(console,'连接错误:'));
+db.once('open',function(){
+//一次打开记录
+});
+var PersonSchema = new mongoose.Schema({
+  name:String   //定义一个属性name，类型为String
+});
+//将该Schema发布为Model
+var PersonModel = db.model('Person',PersonSchema);
+//如果该Model已经发布，则可以直接通过名字索引到，如下：
+//var PersonModel = db.model('Person');
+//如果没有发布，上一段代码将会异常
+
+//用Model创建Entity
+var personEntity = new PersonModel({name:'Krouky'});
+//打印这个实体的名字看看
+console.log(personEntity.name); //Krouky
+
+//我们甚至可以为此Schema创建方法
+//为Schema模型追加speak方法
+/*PersonSchema.methos.speak = function(){
+  console.log('我的名字叫'+this.name);
+}
+var PersonModel = db.model('Person',PersonSchema);
+var personEntity = new PersonModel({name:'Krouky'});
+personEntity.speak();//我的名字叫Krouky
+//Entity是具有具体的数据库操作CRUD的*/
+personEntity.save();  //执行完成后，数据库就有该数据了
+
+//如果要执行查询，需要依赖Model，当然Entity也是可以做到的
+PersonModel.find(function(err,persons){
+//查询到的所有person
+  console.log(persons);
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
